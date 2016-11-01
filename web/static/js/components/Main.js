9616ccfa13f3f9ce1e39c17a490f056d8410ce23
Arthur Parmentier <arthur.jsx@gmail.com>
2016-11-01 19:21:51 +0100

    Added webpack, react and scss

---
+++ web/static/js/components/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div style={{ backgroundColor: 'yellow' }}>
+        Main
+      </div>
+    )
+  }
+}
---
+++ web/static/js/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './containers/App'
+import Main from './components/Main'
+
+ReactDOM.render(
+  <App>
+    <Main />
+  </App>, document.getElementById("index"))
