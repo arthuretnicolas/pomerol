a9c1ee50fb42dce904e75a00ae55d63041e82f26
Arthur Parmentier <arthur.jsx@gmail.com>
2016-12-04 12:19:17 +0100

    Added protected routes related to auth

--- web/static/js/routes/index.js
+++ web/static/js/routes/index.js
@@ -8,14 +8,22 @@ import Contacts from '../Containers/Contacts'
 import GithubUsers from '../Containers/GithubUsers'
 import Signup from '../Containers/Signup'
 import Login from '../Containers/Login'
+import ProtectedView from '../Containers/ProtectedView'
+import ProtectedViewFromUser from '../Containers/ProtectedViewFromUser'
 
 export default (
   <Route path='/' component={AppContainer}>
     <IndexRoute component={Landing} />
-    <Route path='/contacts' component={Contacts} />
-    <Route path='/github-users' component={GithubUsers} />
-    <Route path='/signup' component={Signup} />
-    <Route path='/login' component={Login} />
-    <Route path='*' component={() => <h1>Not found!</h1>} />
+
+    <Route component={ProtectedViewFromUser}>
+      <Route path='/signup' component={Signup} />
+      <Route path='/login' component={Login} />
+    </Route>
+
+    <Route component={ProtectedView}>
+      <Route path='/contacts' component={Contacts} />
+      <Route path='/github-users' component={GithubUsers} />
+      <Route path='*' component={() => <h1>Not found!</h1>} />
+    </Route>
   </Route>
 )
