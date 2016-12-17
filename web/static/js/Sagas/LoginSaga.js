b8b4ee060758001b5e81468c04dd72fc33289bdd
Arthur Parmentier <arthur.jsx@gmail.com>
2016-12-17 13:43:13 +0100

    Fixed bug related to routing with auth

--- web/static/js/Sagas/LoginSaga.js
+++ web/static/js/Sagas/LoginSaga.js
@@ -1,6 +1,5 @@
 import { call, put } from 'redux-saga/effects'
 import LoginActions from '../Reducers/LoginRedux'
-import { browserHistory } from 'react-router'
 
 export function * login (api, action) {
   const { email, password } = action
@@ -8,12 +7,11 @@ export function * login (api, action) {
 
   if (response.ok) {
     yield put(LoginActions.loginSuccess(response.data))
-    browserHistory.push('/dashboard')
   } else {
     yield put(LoginActions.loginFailure())
   }
 }
 
 export function * logout () {
-  browserHistory.push('/login')
+  // nothing to do
 }
