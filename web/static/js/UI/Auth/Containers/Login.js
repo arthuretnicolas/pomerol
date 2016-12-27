f090b4af2cd6e20cc3ea5d4cfda41d726a3fcc55
Arthur Parmentier <arthur.jsx@gmail.com>
2016-12-27 12:21:59 +0100

    Added google login

--- web/static/js/UI/Auth/Containers/Login.js
+++ web/static/js/UI/Auth/Containers/Login.js
@@ -4,9 +4,12 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import FormLogin from '../../Forms/Components/FormLogin'
 import LoginActions from '../../../Reducers/LoginRedux'
+import GoogleLogin from 'react-google-login'
 
 type Props = {
   loginAttempt: () => void,
+  loginWithGoogleSuccess: () => void,
+  loginWithGoogleFailure: () => void,
   attempting: boolean
 }
 
@@ -24,8 +27,17 @@ class Login extends Component {
     })
   }
 
+  _loginWithGoogleSuccess = response =>
+    this.props.loginWithGoogleSuccess(response.accessToken, response.profileObj)
+
+  _loginWithGoogleFailure = error =>
+    this.props.loginWithGoogleFailure(error)
+
   render () {
-    const { loginAttempt, attempting } = this.props
+    const {
+      loginAttempt,
+      attempting
+    } = this.props
     const { email, password } = this.state
 
     return (
@@ -39,6 +51,14 @@ class Login extends Component {
               password
             }}
             attempting={attempting}
+          />
+
+          <GoogleLogin
+            clientId='217330109544-l6gh6agp436gc77i6gqje4t3ni6lluj6.apps.googleusercontent.com'
+            buttonText='Login with Google'
+            onSuccess={this._loginWithGoogleSuccess}
+            onFailure={this._loginWithGoogleFailure}
+            className='googleButton'
           />
         </div>
 
@@ -56,7 +76,11 @@ const mapStateToProps = ({ login }) => ({
 
 const mapDispatchToProps = dispatch => ({
   loginAttempt: (email: string, password: string) =>
-    dispatch(LoginActions.loginAttempt(email, password))
+    dispatch(LoginActions.loginAttempt(email, password)),
+  loginWithGoogleSuccess: (jwt: string, profileObj: Object) =>
+    dispatch(LoginActions.loginWithGoogleSuccess(jwt, profileObj)),
+  loginWithGoogleFailure: (error: Object) =>
+    dispatch(LoginActions.failure(error))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login)
