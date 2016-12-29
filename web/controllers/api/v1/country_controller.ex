ed7fb59448a1f73ddbd064b8f5d74f9ae75efa35
nicolas <nicolas.goumard@gmail.com>
2016-12-29 17:07:57 +0100

    Added top_country_ids to country controller.

--- web/controllers/api/v1/country_controller.ex
+++ web/controllers/api/v1/country_controller.ex
@@ -6,7 +6,9 @@ defmodule Pomerol.V1.CountryController do
   def index(conn, params) do
     locale = conn.assigns[:locale]
     countries = Country |> Country.preload_all(locale) |> Repo.all
+    # TODO : get best matching country with user ip
+    top_country_ids = [1]
     conn
-    |> render(Pomerol.CountryView, "index.json", countries: countries)
+    |> render(Pomerol.CountryView, "index.json", countries: countries, top_country_ids: top_country_ids)
   end
 end
--- web/views/api/v1/country_view.ex
+++ web/views/api/v1/country_view.ex
@@ -1,9 +1,10 @@
 defmodule Pomerol.CountryView do
   use Pomerol.Web, :view
 
-  def render("index.json", %{countries: countries}) do
+  def render("index.json", %{countries: countries, top_country_ids: top_country_ids}) do
     %{
-      countries: render_many(countries, __MODULE__, "country.json")
+      countries: render_many(countries, __MODULE__, "country.json"),
+      top_country_ids: top_country_ids
     }
   end
 
