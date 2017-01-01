778c3b370723e2b220034b58fe53135058744728
Arthur Parmentier <arthur.jsx@gmail.com>
2017-01-01 15:54:21 +0100

    Cleaned

---
+++ config/dev.secrets.exs
@@ -0,0 +1,9 @@
+use Mix.Config
+
+config :ueberauth, Ueberauth.Strategy.Google.OAuth,
+  client_id: "217330109544-l6gh6agp436gc77i6gqje4t3ni6lluj6.apps.googleusercontent.com",
+  client_secret: "ZnlJRbMDka-Upkp7EAaK9l4M"
+
+config :ex_aws,
+  access_key_id: "AKIAJZ3RMHSPREHJ5CGQ",
+  secret_access_key: "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"
