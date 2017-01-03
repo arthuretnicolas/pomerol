1e9385948e4e2599c88c7f79a18f9f6982ae9124
Arthur Parmentier <arthur.jsx@gmail.com>
2017-01-03 16:39:57 +0100

    Added select to change organization

--- web/static/js/UI/Dashboard/Components/DashboardSidebar.js
+++ web/static/js/UI/Dashboard/Components/DashboardSidebar.js
@@ -3,6 +3,30 @@
 import React from 'react'
 import { browserHistory } from 'react-router'
 import Infos from '../../Shared/Components/Infos'
+import Select from '../../Forms/Components/Select'
+
+const ORGANIZATION_MAX_LETTERS = 15
+
+const organizationShortName = organizationName => {
+  if (organizationName.length <= ORGANIZATION_MAX_LETTERS) {
+    return organizationName
+  }
+
+  const shortName = organizationName.substr(organizationName, ORGANIZATION_MAX_LETTERS - 2).trim()
+  return `${shortName}...`
+}
+
+const getSelectMaxWidth = (organizations, selectedOrganizationId) => {
+  const selectedOrganization = organizations.find(org => org.id === selectedOrganizationId)
+
+  if (typeof selectedOrganization === 'undefined') {
+    return 100
+  }
+
+  const nbCharacters = organizationShortName(selectedOrganization.name).length
+
+  return nbCharacters * 10
+}
 
 type Props = {
   data: Array<{
@@ -13,21 +37,55 @@ type Props = {
       iconName: string,
       link: string
     }>
-  }>
+  }>,
+  organizations: Array<Object>,
+  onChange: () => void,
+  selectedOrganizationId: number | null
 }
 
-const OrganisationPicker = () => (
+const OrganisationPicker = ({
+  organizations,
+  onChange,
+  selectedOrganizationId
+}) => (
   <div className='container-organization'>
-    BigCorpo
+    {
+      organizations.length === 1 && <div className='organization-name'>
+        {organizations.length && organizations[0].name}
+      </div>
+    }
+
+    {
+      organizations.length >= 2 && <Select
+        theme='plain'
+        maxWidthSelect={getSelectMaxWidth(organizations, selectedOrganizationId)}
+        selected={selectedOrganizationId}
+        placeholder='Your organization'
+        onChange={event => onChange(event && parseInt(event.target.value))}
+        options={organizations.map(org => ({
+          id: org.id,
+          name: organizationShortName(org.name)
+        }))}
+      />
+    }
   </div>
 )
 
-const DashboardSidebar = ({ data }: Props) => {
+const DashboardSidebar = ({
+  data,
+  organizations,
+  onChange,
+  selectedOrganizationId
+}: Props) => {
   const pathname = browserHistory && browserHistory.getCurrentLocation().pathname
 
   return (
     <div className='Dashboard-DashboardSidebar'>
-      <OrganisationPicker />
+      <OrganisationPicker
+        organizations={organizations}
+        selectedOrganizationId={selectedOrganizationId}
+        onChange={onChange}
+      />
 
       {
         data.map((item, index) => (
