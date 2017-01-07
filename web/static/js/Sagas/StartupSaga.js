c839580cc1fa3234418b79a5f85a7ee8fdfa3896
Arthur Parmentier <arthur.jsx@gmail.com>
2017-01-07 15:55:36 +0100

    Fetch user on load

--- web/static/js/Sagas/StartupSaga.js
+++ web/static/js/Sagas/StartupSaga.js
@@ -1,10 +1,19 @@
-import { put } from 'redux-saga/effects'
+import { put, select } from 'redux-saga/effects'
 import StartupActions from '../Reducers/StartupRedux'
+import LoginActions from '../Reducers/LoginRedux'
+import { jwtSelector, userIdSelector } from '../Services/Selectors'
 
 const isDev = process.env.NODE_ENV === 'development'
 
 // process STARTUP actions
 export function * startup (action) {
+  const jwt = yield select(jwtSelector)
+  const userId = yield select(userIdSelector)
+
+  if (userId) {
+    yield put(LoginActions.fetchSessionAttempt(jwt))
+  }
+
   if (isDev) {
     console.info('startup')
   }
