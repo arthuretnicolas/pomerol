3d33d184b56274f287465285b5b5a9863f9fdbce
nicolas <nicolas.goumard@gmail.com>
2017-01-16 23:44:56 +0100

    Configure mailgun for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -85,9 +85,12 @@ config :logger, level: :info
 #
 
 config :pomerol, Pomerol.Mailer,
-  adapter: Bamboo.MailgunAdapter,
-  api_key: System.get_env("MAILGUN_API_KEY"),
-  domain: System.get_env("MAILGUN_DOMAIN")
+  adapter: Bamboo.LocalAdapter
+
+# config :pomerol, Pomerol.Mailer,
+#   adapter: Bamboo.MailgunAdapter,
+#   api_key: System.get_env("MAILGUN_API_KEY"),
+#   domain: System.get_env("MAILGUN_DOMAIN")
 
 # Configure your database
 config :pomerol, Pomerol.Repo,
