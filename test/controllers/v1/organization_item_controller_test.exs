e1e5010f572df623965a145a2f7e3de542b80495
nicolas <nicolas.goumard@gmail.com>
2017-01-24 11:15:30 +0100

    Added and improved tests.

--- test/controllers/v1/organization_item_controller_test.exs
+++ test/controllers/v1/organization_item_controller_test.exs
@@ -1,4 +1,47 @@
 defmodule Pomerol.V1.OrganizationItemControllerTest do
   use Pomerol.ApiCase
-  # TODO
+
+  describe "create a text-item" do
+    test "returns 401 when user is not authenticated", %{conn: conn} do
+      organization = insert(:organization)
+      conn = post conn, "/api/v1/organizations/#{organization.id}/items", %{type: "text-item", title: "TITLE"}
+      json = conn |> json_response(401)
+    end
+
+    @tag :authenticated
+    test "returns 403 when user is not a member of the org", %{conn: conn, current_user: current_user} do
+      organization = insert(:organization)
+      conn = post conn, "/api/v1/organizations/#{organization.id}/items", %{type: "text-item", title: "TITLE"}
+      json = conn |> json_response(403)
+    end
+
+    @tag :authenticated
+    test "returns 403 when user is viewer of the org", %{conn: conn, current_user: current_user} do
+      organization = insert(:organization)
+      membership = insert(:organization_membership, organization: organization, member: current_user, role: "viewer")
+      conn = post conn, "/api/v1/organizations/#{organization.id}/items", %{type: "text-item", title: "TITLE"}
+      json = conn |> json_response(403)
+    end
+
+    @tag :authenticated
+    test "returns 422 when user is owner of the org and type is missing", %{conn: conn, current_user: current_user} do
+      organization = insert(:organization)
+      membership = insert(:organization_membership, organization: organization, member: current_user, role: "owner")
+      conn = post conn, "/api/v1/organizations/#{organization.id}/items", %{title: "TITLE"}
+      json = conn |> json_response(422)
+      assert json["errors"] != %{}
+    end
+
+    @tag :authenticated
+    test "returns 201 when an owner tries to create a text-item with valid attrs", %{conn: conn, current_user: current_user} do
+      organization = insert(:organization)
+      membership = insert(:organization_membership, organization: organization, member: current_user, role: "owner")
+      conn = post conn, "/api/v1/organizations/#{organization.id}/items", %{type: "text-item", title: "TITLE OF THE ITEM", description: "Description"}
+      json = conn |> json_response(201)
+
+      assert json["type"] == "text-item"
+      assert json["organization_id"] == organization.id
+      assert json["title"] == "TITLE OF THE ITEM"
+    end
+  end
 end
--- test/models/organization_item_test.exs
+++ test/models/organization_item_test.exs
@@ -1,4 +1,54 @@
 defmodule Pomerol.OrganizationItemTest do
   use Pomerol.ModelCase
-  # TODO
+
+  alias Pomerol.OrganizationItem
+
+  describe "create_changeset for text item" do
+    test "with valid attributes" do
+      attrs =
+          %{type: "text-item", title: "TITLE"}
+          |> Map.put(:organization_id, "56582bfb-ece4-4a46-8481-7dfab79d7785")
+      changeset = OrganizationItem.create_changeset(%OrganizationItem{}, attrs)
+      assert changeset.valid?
+    end
+
+    test "with invalid attributes" do
+      changeset = OrganizationItem.create_changeset(%OrganizationItem{}, %{type: "text-item"})
+      refute changeset.valid?
+    end
+  end
+
+  describe "create_changeset for not-optional price-item" do
+    test "with valid attributes" do
+      attrs =
+          %{type: "price-item", title: "TITLE", code: "InternalId", option_type: "not-optional", editable_quantity: false}
+          |> Map.put(:organization_id, "56582bfb-ece4-4a46-8481-7dfab79d7785")
+          |> Map.put(:tax_rate_id, "56582bfb-ece4-4a46-8481-7dfab79d7785")
+          |> Map.put(:sales_category_id, "56582bfb-ece4-4a46-8481-7dfab79d7785")
+      changeset = OrganizationItem.create_changeset(%OrganizationItem{}, attrs)
+      assert changeset.valid?
+    end
+  end
+
+  describe "create_changeset for optional price-item" do
+    test "with valid attributes" do
+      attrs =
+          %{type: "price-item", title: "TITLE", code: "InternalId", option_type: "optional", selected: false, editable_quantity: false}
+          |> Map.put(:organization_id, "56582bfb-ece4-4a46-8481-7dfab79d7785")
+          |> Map.put(:tax_rate_id, "56582bfb-ece4-4a46-8481-7dfab79d7785")
+          |> Map.put(:sales_category_id, "56582bfb-ece4-4a46-8481-7dfab79d7785")
+      changeset = OrganizationItem.create_changeset(%OrganizationItem{}, attrs)
+      assert changeset.valid?
+    end
+
+    test "with invalid attributes" do
+      attrs =
+          %{type: "price-item", title: "TITLE", code: "InternalId", option_type: "optional", editable_quantity: false}
+          |> Map.put(:organization_id, "56582bfb-ece4-4a46-8481-7dfab79d7785")
+          |> Map.put(:tax_rate_id, "56582bfb-ece4-4a46-8481-7dfab79d7785")
+          |> Map.put(:sales_category_id, "56582bfb-ece4-4a46-8481-7dfab79d7785")
+      changeset = OrganizationItem.create_changeset(%OrganizationItem{}, attrs)
+      refute changeset.valid?
+    end
+  end
 end
